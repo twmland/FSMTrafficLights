3b468be9c828bd7fb2b5f544334afce2233a1820
King Liu <64719094+twmland@users.noreply.github.com>
2020-05-08 17:02:56 +0800

    Define MachineState interface

---
+++ FSMTrafficLights/IStateMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSMTrafficLights
+{
+    public interface IStateMachine
+    {
+        void Entry();
+        void Exit();
+        void Input();
+        void Transfer();
+    }
+}
